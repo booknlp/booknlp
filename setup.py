2b42ccd40dc2c62097308398d4e08f91ecab4177
David Bamman <david.bamman@gmail.com>
2021-12-03 15:54:01 -0800

    1.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='booknlp', 
-	version='1.0.6', 
+	version='1.0.7', 
 	packages=find_packages(),
 	py_modules=['booknlp'],
 	url="https://github.com/dbamman/book-nlp",
