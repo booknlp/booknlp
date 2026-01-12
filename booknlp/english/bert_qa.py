f6f77d7c9948e2ca80b2a11c0f86fe4863ef496e
Jacob Hales <halesjacoba@gmail.com>
2026-01-11 21:01:01 -0700

    fix: use os.path.basename() for cross-platform path handling
    Replaces modelFile.split('/')[-1] with os.path.basename() which works correctly on both Windows and Unix. Fixes #12

--- booknlp/english/bert_qa.py
+++ booknlp/english/bert_qa.py
@@ -1,5 +1,6 @@
 import torch
 import re
+import os
 from booknlp.english.speaker_attribution import BERTSpeakerID
 import numpy as np
 import sys
@@ -13,7 +14,7 @@ class QuotationAttribution:
 
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-		base_model=re.sub("google_bert", "google/bert", modelFile.split("/")[-1])
+		base_model=re.sub("google_bert", "google/bert", os.path.basename(modelFile))
 		base_model=re.sub(".model", "", base_model)
 
 		self.model = BERTSpeakerID(base_model=base_model)
